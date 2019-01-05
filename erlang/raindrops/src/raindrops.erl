2426511667fdc7f65d58ad48a3c786abb2a333a3
Savas Aydin <savas.aydin@ericsson.com>
2019-01-05 17:51:24 +0100

    Removes duplication of factors.
    Both the macro defination and the get_output/1 funtions
    uses the factor numbers. Remove this duplications by coupling
    the factors with their sounds.
    
    It is now only one place to add/remove a new factor with its sound.

--- erlang/raindrops/src/raindrops.erl
+++ erlang/raindrops/src/raindrops.erl
@@ -2,23 +2,16 @@
 
 -export([convert/1]).
 
--define(FACTORS, [3, 5, 7]).
+-define(FACTORS, [{3, "Pling"}, {5, "Plang"}, {7, "Plong"}]).
 
 convert(Number) ->
     convert(Number, ?FACTORS, "").
 
-convert(Number, [F|Factors], Acc) when Number rem F == 0 ->
-    convert(Number, Factors, Acc ++ get_output(F));
+convert(Number, [{F, O}|Factors], Acc) when Number rem F == 0 ->
+    convert(Number, Factors, Acc ++ O);
 convert(Number, [_|Factors], Acc) ->
     convert(Number, Factors, Acc);
 convert(Number, [], "") ->
     integer_to_list(Number);
 convert(_, [], Output) ->
     Output.
-
-get_output(3) ->
-    "Pling";
-get_output(5) ->
-    "Plang";
-get_output(7) ->
-    "Plong".
